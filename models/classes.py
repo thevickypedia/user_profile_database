385d84a5b506dad95b99e0a70aff26057fef9784
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-11-21 12:26:19 -0600

    Remove references from `api_file_handler`

--- models/classes.py
+++ models/classes.py
@@ -1,52 +1,6 @@
-from os import getcwd
-from typing import Optional
-
 from passlib.hash import bcrypt
-from pydantic import BaseModel
 from tortoise import fields
 from tortoise.models import Model
-
-
-class DownloadHandler(BaseModel):
-    """BaseModel that handles input data for the API which is treated as members for the class ``DownloadHandler``.
-
-    >>> DownloadHandler
-
-    """
-
-    FileName: str
-    FilePath: str = getcwd()
-
-
-class UploadHandler(BaseModel):
-    """BaseModel that handles input data for the API which is treated as members for the class ``UploadHandler``.
-
-    >>> UploadHandler
-
-    """
-
-    FileName: Optional[str]
-    FilePath: str = getcwd()
-
-
-class GetPhrase(BaseModel):
-    """BaseModel that handles input data for the API which is treated as members for the class ``GetPhrase``.
-
-    >>> GetPhrase
-
-    """
-
-    apikey: str
-
-
-class Bogus(Model):
-    """Model that handles input data for the API which is treated as members for the class ``Bogus``.
-
-    >>> Bogus
-
-    """
-
-    authentication: str = fields.CharField(50)
 
 
 class Login(Model):
